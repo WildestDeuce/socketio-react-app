128da05d61f66c166e38a1cbd4a7a628bc3b5a93
Charles Mercer <c.e.mercer15@gmail.com>
2019-10-15 20:48:05 -0400

    added handle events to server file

--- server.js
+++ server.js
@@ -23,33 +23,67 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglis
 
 // socket connection
 io.on("connection", (client) => {
+    let chat = db.collection("chats");
     client.on("subscribeToTimer", (interval) => {
         console.log("client is subscribing to timer with interval", interval);
         setInterval(() => {
             client.emit("timer", new Date());
         }, interval);
+
+        chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
+            if (err) {
+                throw err;
+            }
+            //Emit the message
+            client.emit("output", res)
+        })
     })
 
+    //Handle input events
 
+    client.on("input", function (data) {
+        let name = data.name;
+        let message = data.message;
 
-    client.on("join", handlers.handleJoin);
+        //Check for name and message
+        if (name == "" || message == "") {
+            //Send error status
+            sendStatus("Please enter name and message");
+        } else {
+            //Insert message
+            chat.insert({ name: name }, {message: message}, function() {
+                io.emit("output", [data]);
 
-    client.on("leave", handlers.handleLeave);
+                //Send status object
+                sendStatus({
+                    message: "Message sent",
+                    clear: true
+                })
+            });
 
-    client.on("message", handlers.handleMessage);
+        }
+    })
 
-    client.on("chatrooms", handlers.handleGetChatrooms);
+}
 
-    client.on("disconnect", function () {
-        console.log("client disconnect...", client.id);
-        handleDisconnect()
-    });
+client.on("join", handlers.handleJoin);
 
-    client.on("error", function (err) {
-        console.log("received error rom client:", client.id);
-        console.log(err)
-    });
-})
+client.on("leave", handlers.handleLeave);
+
+client.on("message", handlers.handleMessage);
+
+client.on("chatrooms", handlers.handleGetChatrooms);
+
+client.on("disconnect", function () {
+    console.log("client disconnect...", client.id);
+    handleDisconnect()
+});
+
+client.on("error", function (err) {
+    console.log("received error rom client:", client.id);
+    console.log(err)
+});
+
 
 // Start the API server
 server.listen(PORT, function () {
